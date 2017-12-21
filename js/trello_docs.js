06ccd7932ff19c292c9c36525bc7fb1231d1d35e
Paulo Rocha <paulochrocha@gmail.com>
2017-12-21 14:54:05 -0200

    add comments and ga

--- js/trello_docs.js
+++ js/trello_docs.js
@@ -70,9 +70,9 @@ var listBoards=function(){
 			var list={};
 			list.boards=value;
 			if(key===""||key===null){
-				list.name="Personal";
+				list.name="Pessoa;";
 			}else if(!orgList.hasOwnProperty(key)){
-				list.name="External Organization";
+				list.name="Organização Externa";
 			}else{
 				list.name=orgList[key][0].displayName
 			}
@@ -92,111 +92,126 @@ var listBoards=function(){
 };
 
 var getBoard=function(board){
+
   $("#view").empty();
   $("#view").html("<h1>Carregando ...</h1>");
-  Trello.get("/boards/"+board,{cards:"open",lists:"open",checklists:"all",members:"all"},function(board){
-	$("#view").html("<h1>Carregando ...OK!!</h1>");
-	window.doc=board; //debug
-	window.title=board.name;
-	_.each(board.cards,function(card){ //iterate on cards
-		_.each(card.idChecklists,function(listId){ //iterate on checklists
-			var list=_.find(board.checklists,function(check){ //Find list
-				return check.id==listId;
+  Trello.get("/boards/"+board+"/actions",{filter:"commentCard",limit:"5"},function(comments){
+	  console.log(comments);
+	  Trello.get("/boards/"+board,{cards:"open",lists:"open",checklists:"all",members:"all", comments: "all"},function(board){
+			$("#view").html("<h1>Carregando ...OK!!</h1>");
+			window.doc=board; //debug
+			window.title=board.name;
+			_.each(board.cards,function(card){ //iterate on cards
+				_.each(card.idChecklists,function(listId){ //iterate on checklists
+					var list=_.find(board.checklists,function(check){ //Find list
+						return check.id==listId;
+						});
+					if(!list){
+						console.log("ERRO:"+listId+" não encontrado");
+						return;
+					}
+					list.doneNumber=0;
+					list.totalNumber=list.checkItems.length || 0;
+					_.each(list.checkItems,function(item){ //Check complete
+						if(item.state=="complete"){
+							list.doneNumber++;
+							item.complete=true;
+						}else item.complete=false;
+					});
+					list.done=(list.doneNumber==list.totalNumber);
+					var template="<div><b>{{name}}</b> <span class='show right {{#done}}green{{/done}}'>{{doneNumber}}/{{totalNumber}}</span></div><ul>{{#checkItems}}<li>{{#complete}}<del>{{/complete}}{{name}}{{#complete}}</del>{{/complete}}</li>{{/checkItems}}</ul>";
+					var str=Mustache.render(template,list);
+
+					card.checklist=card.checklist||[]; //Make array
+					card.checklist.push(str);
+				});//iterate on checklists
+
+				card.members=_.map(card.idMembers,function(id){ // iterate on members
+					var member=_.find(board.members, function(m) {
+						return m.id==id;
+					});
+					return member.fullName;
+				});// iterate on members
+				card.comments = [];
+				_.each(comments, function (comment) { //iterate on comments
+						if(card.id === comment.data.card.id){
+							card.comments.push(comment.data.text);
+						}
 				});
-			if(!list){
-				console.log("ERRO:"+listId+" não encontrado");
-				return;
-			}
-			list.doneNumber=0;
-			list.totalNumber=list.checkItems.length || 0;
-			_.each(list.checkItems,function(item){ //Check complete
-				if(item.state=="complete"){
-					list.doneNumber++;
-					item.complete=true;
-				}else item.complete=false;
+
+			});//iterate on cards
+
+			// Second Init Cards
+			var listofcards=_.groupBy(board.cards, function(card){
+				return card.idList;
 			});
-			list.done=(list.doneNumber==list.totalNumber);
-			var template="<div><b>{{name}}</b> <span class='show right {{#done}}green{{/done}}'>{{doneNumber}}/{{totalNumber}}</span></div><ul>{{#checkItems}}<li>{{#complete}}<del>{{/complete}}{{name}}{{#complete}}</del>{{/complete}}</li>{{/checkItems}}</ul>";
-			var str=Mustache.render(template,list);
-
-			card.checklist=card.checklist||[]; //Make array
-			card.checklist.push(str);
-		});//iterate on checklists
-
-		card.members=_.map(card.idMembers,function(id){ // iterate on members
-			var member=_.find(board.members, function(m) {
-				return m.id==id;
+			_.each(board.lists,function(list){
+				list.cards=listofcards[list.id];
+				list.size=list.cards?list.cards.length:0;
+				list.show=(list.size>0);
 			});
-			return member.fullName;
-		});// iterate on members
-	});//iterate on cards
+			console.log(board);
 
-	// Second Init Cards
-	var listofcards=_.groupBy(board.cards, function(card){
-		return card.idList;
-	});
-	_.each(board.lists,function(list){
-		list.cards=listofcards[list.id];
-		list.size=list.cards?list.cards.length:0;
-		list.show=(list.size>0);
-	});
-	console.log(board);
+			// Date function
+			board.formatDate=function(){
+				return function(text){
+					var date;
+					switch(text){
+					case "":
+						return "Nenhuma";
+					case "now":
+						date=new Date();
+						break;
+					default:
+						date=new Date(text);
+					}
+					return moment(date).format("DD/MM/YYYY");//date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
+				};
+			};
+			board.formatComments=function(){
+				var converter = new Showdown.converter();
+				return converter.makeHtml;
+			};		
+			//
+			// Start Rendering
+			board.displayColumns=["Nome","Descrição","Últimos Comentários","Data Limite","Checklists","Membros","Labels"];
+			var htmltemplate="<h1><span id='trello-link'></span><span id='printme'></span>{{name}} <span class='right'>{{#formatDate}}now{{/formatDate}}</span></h1>{{#lists}}<table><caption><h2>{{name}} <span class='show right'>{{size}}</span></h2></caption>{{#show}}<col width='20%' /><col width='20%' /><col width='20%' /><col width='5%' /><col width='20%' /><col width='10%' /><col width='5%' /><thead><tr>{{#displayColumns}}<th scope='col'>{{.}}</th>{{/displayColumns}}</tr></thead>{{/show}}<tbody>{{#cards}}<tr><td scope='row'><b>{{name}}</b></td><td><div class='comments'>{{#formatComments}}{{desc}}{{/formatComments}}</div></td><td>{{#comments}}<div class='comment'>{{.}}</div>{{/comments}}</td><td>{{#formatDate}}{{due}}{{/formatDate}}</td><td>{{#checklist}}<div>{{{.}}}</div>{{/checklist}}</td><td>{{#members}}<div class='member'>{{.}}</div>{{/members}}</td><td>{{#labels}}<div class='show {{color}}'>{{name}}&nbsp;</div>{{/labels}}</td></tr>{{/cards}}</tbody></table>{{/lists}}";
+			var csvtemplate="";//TODO
 
-	// Date function
-	board.formatDate=function(){
-		return function(text){
-			var date;
-			switch(text){
-			case "":
-				return "Nenhuma";
-			case "now":
-				date=new Date();
-				break;
-			default:
-				date=new Date(text);
-			}
-			return date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
-		};
-	};
-	board.formatComments=function(){
-		var converter = new Showdown.converter();
-		return converter.makeHtml;
-	};		
-	//
-	// Start Rendering
-	board.displayColumns=["Nome","Descrição","Data Limite","Checklists","Membros","Labels"];
-	var htmltemplate="<h1><span id='trello-link'></span><span id='printme'></span>{{name}} <span class='right'>{{#formatDate}}now{{/formatDate}}</span></h1>{{#lists}}<table><caption><h2>{{name}} <span class='show right'>{{size}}</span></h2></caption>{{#show}}<col width='20%' /><col width='30%' /><col width='5%' /><col width='25%' /><col width='5%' /><col width='10%' /><col width='5%' /><thead><tr>{{#displayColumns}}<th scope='col'>{{.}}</th>{{/displayColumns}}</tr></thead>{{/show}}<tbody>{{#cards}}<tr><td scope='row'><b>{{name}}</b></td><td><div class='comments'>{{#formatComments}}{{desc}}{{/formatComments}}</div></td><td>{{#formatDate}}{{due}}{{/formatDate}}</td><td>{{#checklist}}<div>{{{.}}}</div>{{/checklist}}</td><td>{{#members}}<div>{{.}}</div>{{/members}}</td><td>{{#labels}}<div class='show {{color}}'>{{name}}&nbsp;</div>{{/labels}}</td></tr>{{/cards}}</tbody></table>{{/lists}}";
-	var csvtemplate="";//TODO
+			var str=Mustache.render(htmltemplate,board);
+			$("#view").html(str);
 
-	var str=Mustache.render(htmltemplate,board);
-	$("#view").html(str);
+			// Download Button
+			var download="<!DOCTYPE html><html><head><meta charset='utf-8' /><title>"+board.name+"</title><style>"+$("style").text()+"</style></head><body>"+str+"</body></html>";
+			
+			var button1=$("#download");
+			button1.addClass("downloader");
+			button1.text("Save HTML");
+			button1.click(function(){
+				console.log("saving..");
+				var bb=new BlobBuilder();
+				bb.append(download);
+				var filesaver=saveAs(bb.getBlob("text/html;charset=utf-8"),board.name+"_"+board.formatDate()('now')+".html");
+			});
+			
+			var button2=$("#trello-link");
+			button2.addClass("downloader");
+			button2.text("Trello");
+			button2.click(function(){
+				location=board.url;
+			});
 
-	// Download Button
-	var download="<!DOCTYPE html><html><head><meta charset='utf-8' /><title>"+board.name+"</title><style>"+$("style").text()+"</style></head><body>"+str+"</body></html>";
-//this may work for firefox using application/data
-//location.href="data:text/html;charset=utf-8,"+encodeURIComponent(download);
-	var button1=$("#download");
-	button1.addClass("downloader");
-	button1.text("Save HTML");
-	button1.click(function(){
-		console.log("saving..");
-		var bb=new BlobBuilder();
-		bb.append(download);
-		var filesaver=saveAs(bb.getBlob("text/html;charset=utf-8"),board.name+"_"+board.formatDate()('now')+".html");
-	});
-		var button2=$("#trello-link");
-	button2.addClass("downloader");
-	button2.text("Trello");
-	button2.click(function(){
-		location=board.url;
-	});
-	var button3=$("#printme");
-	button3.addClass("downloader");
-	button3.text("Imprimir");
-	button3.click(function(){
-		print();
-	});
+			var button3=$("#printme");
+			button3.addClass("downloader");
+			button3.text("Imprimir");
+			button3.click(function(){
+				print();
+			});
 
-	//button.click(function(){location.href="data:text/html;charset=utf-8,"+encodeURIComponent(download);});
-	});
+		});
+
+  });
+ 
+ 
+  
 };
